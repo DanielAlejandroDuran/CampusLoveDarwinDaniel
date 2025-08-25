b541fd774da43a1312b434d7ccf3c4907e205d19
DarwinArenas <felipearenascarvajal123@gmail.com>
2025-08-25 14:30:45 -0500

    .

--- obj/Debug/net9.0/CampusLove.AssemblyInfo.cs
+++ obj/Debug/net9.0/CampusLove.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusLove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+641cf9c17eafbe1b36ab676631a1dc98f93a0fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de2767419c04d766eb8a2ead475929f020936b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusLove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusLove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
